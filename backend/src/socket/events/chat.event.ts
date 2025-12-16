d4c8cd46d584c5a49eb214690ab485e95acf8a81
copyniinja <killfyz0.o@gmail.com>
2025-12-16 21:58:43 +0600

    initialize frontend

--- backend/src/socket/events/chat.event.ts
+++ backend/src/socket/events/chat.event.ts
@@ -2,6 +2,7 @@ import { Server, Socket } from "socket.io";
 
 export default function chatEvents(io: Server, socket: Socket) {
   socket.on("chat:send", (payload) => {
+    console.log(payload);
     // Emit the event with massage data
     socket.to(payload.roomId).emit("chat:receive", payload);
   });
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-amber-400">
+        <p>Hello world</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
